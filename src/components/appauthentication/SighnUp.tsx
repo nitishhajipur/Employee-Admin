5468a1b5fa2aa4a08c79edeba588307e11fa3584
shanker1997 <gourishanker697@gmail.com>
2024-01-06 15:44:52 +0530

    sighnups

--- src/components/appauthentication/SighnUp.tsx
+++ src/components/appauthentication/SighnUp.tsx
@@ -1,8 +1,19 @@
 import Reacrt from 'react'
+import './styles.css'
+import { useNavigate } from 'react-router'
 const SighnUp=(props:any)=>{
+    const navigate=useNavigate()
     return(
         <>
-        <p>Sighn up</p>
+        <div className='sighnUpContaoner'>
+            <div className='formContainer'>
+            <div className='backToSighnIn' onClick={(e:any)=>navigate('/')}>
+                <span>Back to SighnIn</span>
+            </div>
+                <p>sighnup</p>
+
+            </div>
+        </div>
         </>
     )
 
