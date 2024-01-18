27fa4ade24fdf9c9cb57b3dde876bf19138a3a99
shanker1997 <gourishanker697@gmail.com>
2024-01-18 08:38:32 +0530

    stores and commonactions of users

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -1,10 +1,13 @@
+import { UserInfo } from "../constants/userPageContants/userDataType"
 import { appTypes } from "./types"
 
-const initialState={boolean:true}
+const initialState={
+    allUserData:null,
+}
 export const Application=(state=initialState,action:any)=>{
     switch(action.type){
-        case appTypes.TestBoolean:
-            return{...state,boolean:action.payload}
+        case appTypes.ALL_USER_DATA:
+            return{...state,allUserData:action.payload}
         
         default:
             return{...state}
