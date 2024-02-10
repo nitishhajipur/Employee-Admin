da8d93ab771df49e382da58a91b3a33b41678471
shanker1997 <gourishanker697@gmail.com>
2024-02-10 10:10:45 +0530

    cha

--- src/components/modules/UserProfile/UserProfile.tsx
+++ src/components/modules/UserProfile/UserProfile.tsx
@@ -5,8 +5,8 @@ function UserProfile() {
   const [src,setSrc]=useState('https://static.tnn.in/thumb/msid-107500992,thumbsize-13174,width-1280,height-720,resizemode-75/107500992.jpg')
 
   const uploadProfilePic=(files:any)=>{
-    // alert("image upload in process")
-    setSrc('https://i.pinimg.com/564x/ac/55/47/ac5547f0bd276cbbad2fb5aa6cf8cf34.jpg')
+    alert("image upload in process")
+    // setSrc('https://i.pinimg.com/564x/ac/55/47/ac5547f0bd276cbbad2fb5aa6cf8cf34.jpg')
     console.log(files,"8files")
   }
   return (
@@ -24,8 +24,8 @@ function UserProfile() {
             </span>
           </label>
           </div>
-          <p className='u-Name m-0'>Gouri Shanker</p>
-          <p className='m-0'> App Admin</p>        
+          <p className='u-Name m-0'>User name</p>
+          {/* <p className='m-0'> App Admin</p>         */}
           </div>
       </div>
       <div className='col-12 gap -3 mt-1 b-container justify-content-between d-flex'>
